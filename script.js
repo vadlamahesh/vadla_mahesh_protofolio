f7ea96662fbe5d31df26c8114833eedbc17c3633
VADLA MAHESH <164859512+vadlamahesh@users.noreply.github.com>
2026-09-09 12:32:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+const menu=document.querySelector('.menu');const nav=document.querySelector('.navbar nav');menu.addEventListener('click',()=>nav.classList.toggle('open'));document.querySelectorAll('nav a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));
